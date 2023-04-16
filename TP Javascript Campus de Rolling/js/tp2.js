6e0e15e4f9dc2e37107a16a0b85237c3db16b0cc
Nicolas Maggia <nickmaggia20@gmail.com>
2023-04-16 14:13:43 -0300

    cargando TP2 y ejercicios 1 y 2

---
+++ TP Javascript Campus de Rolling/js/tp2.js
@@ -0,0 +1,45 @@
+
+/* 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje. */
+
+/* let edad = parseInt(prompt('Ingresa tu edad.'));
+
+if (edad >= 18){
+    document.write(`Ya puede conducir.`)
+}else{
+    document.write('La edad ingresa no es valida. ')
+}*/
+
+/* 2- Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+Ejemplo:
+Input: 5
+Input: 50
+Input: hola10
+Output: Suficiente
+Output: Número erróneo
+Output: Introduce un número válido
+ */
+
+let nota = parseInt(prompt('Ingrese una nota. '));
+
+if(nota <= 2 ){
+    document.write(`Muy deficiente. `);
+}else if(nota > 2 && nota < 5){
+    document.write('Insuficiente.');
+}else if(nota >= 5 && nota < 7){
+    document.write(`Suficiente. `);
+}else if(nota == 7){
+    document.write('Bien.');
+}else if(nota > 7 && nota < 10){
+    document.write('Notable. ');
+}else if(nota == 10){
+    document.write('Sobresaliente.');
+}else{
+    document.write('El numero ingresado no es valido. ');
+}
