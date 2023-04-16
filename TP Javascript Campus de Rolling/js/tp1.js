dcf8090158d7a68b7df42723a3d442961ffa24c2
Nicolas Maggia <nickmaggia20@gmail.com>
2023-04-16 13:54:33 -0300

    queda pendiente el ejercicio 9 del primer trabajo practico

--- TP Javascript Campus de Rolling/js/tp1.js
+++ TP Javascript Campus de Rolling/js/tp1.js
@@ -80,3 +80,56 @@ if(num % 2 == 0 && num > 0 ){
 }
 
  */
+
+/* 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:
+input: Hola mundo
+Output: oauo
+ */
+
+/* let frase = (prompt('Escribe una frase.')); */
+
+
+/* 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+Ejemplo: 
+input: 20
+Output: El 20 es divisible por 2. */
+
+/* let num = parseInt(prompt('Ingrese un numero.'));
+
+if(num % 2 == 0 && num > 0 ){
+    document.write(`${num} es divisible por 2. `);
+}else if(num % 3 == 0 && num > 0){
+    document.write(`${num} es divisible por 3. `);
+}else if(num % 5 == 0 && num > 0 ){
+    document.write(`${num} es divisible por 5. `);
+}else if(num % 7 == 0 && num > 0 ){
+    document.write(`${num} es divisible por 7. `);
+} else{
+    document.write(`El numumero ingresado no es valido. `)
+}
+ */
+
+/* 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+Ejemplo: 
+input: 20
+input: 210
+Output: El 20 es divisible por 2 y por 5.
+Output: El 210 es divisible por 2, por 3, por 5 y por 7. */
+
+
+let num = parseInt(prompt('Ingrese un numero. '));
+
+if(num % 2 == 0 && num > 0 ){
+    document.write(`${num} es divisible por 2. <br>`);
+} 
+if(num % 5 == 0 && num > 0){
+    document.write(`${num} es divisible por 5. <br>`);
+}
+if(num % 3 == 0 && num > 0){
+    document.write(`${num} es divisible por 3. <br>`);
+}
+if(num % 7 == 0 && num > 0 ){
+    document.write(`${num} es divisible por 7. <br>`);
+} 
